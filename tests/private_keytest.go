b29a009881d3875eb254eb2e258529a9a3b9f1fa
Tanuj Soni <tanuj0902@gmail.com>
2017-08-21 16:27:08 +0000

    Change: extract json from structs in tests

--- tests/private_keytest.go
+++ tests/private_keytest.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"encoding/json"
 	"github.com/revel/revel/testing"
+	"io"
 	"strings"
 )
 
@@ -10,62 +11,56 @@ type PrivateKeyTest struct {
 	testing.TestSuite
 }
 
-type MnemonicKeyTest struct {
+type mnemonicKeyTest struct {
 	Key      string `json:"key"`
 	Mnemonic string `json:"mnemonic"`
 }
 
+type mnemonicKeyJsonParam struct {
+	Id       string `json:"id"`
+	Password string `json:"password"`
+}
+
+func testParamsAsJsonReader(id, password string) io.Reader {
+	a, _ := json.Marshal(&mnemonicKeyJsonParam{id, password})
+	return strings.NewReader(string(a))
+}
+
 func (t *PrivateKeyTest) TestCreateActionValidationWorksProperly() {
-	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", strings.NewReader("{}")).Send()
+
+	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", testParamsAsJsonReader("", "")).Send()
 	t.AssertStatus(422)
 	t.AssertContains("Id is required.")
 	t.AssertContains("Password is required.")
 
-	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", strings.NewReader(
-		`{"id": "", "password": ""}`,
-	)).Send()
-	t.AssertStatus(422)
-	t.AssertContains("Id is required.")
-	t.AssertContains("Password is required.")
-
-	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", strings.NewReader(
-		`{"id": "", "password": "abcd"}`,
-	)).Send()
+	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", testParamsAsJsonReader("", "abcd")).Send()
 	t.AssertStatus(422)
 	t.AssertContains("Id is required.")
 	t.AssertContains("Password must be at least 8 characters.")
 
-	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", strings.NewReader(
-		`{"id":"test", "password":"abcd"}`,
-	)).Send()
+	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", testParamsAsJsonReader("test", "abcd")).Send()
 	t.AssertStatus(422)
 	t.AssertNotContains("Id is required.")
 	t.AssertContains("Password must be at least 8 characters.")
 
-	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", strings.NewReader(
-		`{"id": "test", "password": "abcdefgh"}`,
-	)).Send()
+	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", testParamsAsJsonReader("test", "abcdefgh")).Send()
 	t.AssertStatus(200)
 	t.AssertNotContains("Id is required.")
 	t.AssertNotContains("Password must be at least 8 characters.")
 
-	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", strings.NewReader(
-		`{"id": "test", "password": "abcdefghasodijfasdlifjadsoifjaodsifjdsaifjadosifjasdoifjsdfaidsfjasiodjfasiodfjasdoifj"}`,
-	)).Send()
+	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", testParamsAsJsonReader("test", "abcdefghasodijfasdlifjadsoifjaodsifjdsaifjadosifjasdoifjsdfdsfsdf")).Send()
 	t.AssertStatus(422)
 	t.AssertNotContains("Id is required.")
 	t.AssertContains("Password must be at most 64 characters.")
 }
 
 func (t *PrivateKeyTest) TestCreateActionGereratesKeyAndMneonics() {
-	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", strings.NewReader(
-		`{"id": "12345", "password": "abcdefgh"}`,
-	)).Send()
+	t.PostCustom((t.BaseUrl() + "/private_key"), "application/json", testParamsAsJsonReader("12345", "abcdefgh")).Send()
 	t.AssertOk()
 	t.AssertContentType("application/json; charset=utf-8")
 
 	// Parsing the response from server.
-	var response MnemonicKeyTest
+	var response mnemonicKeyTest
 	err := json.Unmarshal(t.ResponseBody, &response)
 	t.Assert(err == nil)
 
